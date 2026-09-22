c22d0fc5968aeaa9289b99c34f4a6e956fb43719
Israel Damiekumo <81820172+MrPie2@users.noreply.github.com>
2026-09-22 16:20:22 -0700

    Fix investment plan features JSON casting

--- app/Models/InvestmentPlan.php
+++ app/Models/InvestmentPlan.php
@@ -7,13 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class InvestmentPlan extends Model
 {
     protected $table = 'investment_plans';
+
     protected $guarded = [];
+
     protected $casts = [
         'id' => 'integer',
         'minimum_amount' => 'float',
         'maximum_amount' => 'float',
         'illustrative_rate' => 'float',
-        'featured' => 'integer',
+        'featured' => 'boolean',
+        'features' => 'array',
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
