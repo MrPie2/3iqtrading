69d8f30b6d9f0d21f6aed25bfd42694de05bfe02
Israel Damiekumo <81820172+MrPie2@users.noreply.github.com>
2026-09-23 02:27:08 -0700

    Use production assets instead of Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,5 +37,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
     @stack('scripts')
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/js/app.js') }}?v={{ filemtime(public_path('assets/js/app.js')) }}"></script>
 </body>
 </html>
