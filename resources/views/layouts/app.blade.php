5961ebed229e84635009441d2b4c816cc5b68940
Israel Damiekumo <81820172+MrPie2@users.noreply.github.com>
2026-09-22 17:57:28 -0700

    clean public layout head structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
 <head>
-     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
