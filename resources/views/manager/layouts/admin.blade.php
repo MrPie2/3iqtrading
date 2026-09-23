2b0fa6d4619db1100a37335fdee135f0f40ed138
Israel Damiekumo <81820172+MrPie2@users.noreply.github.com>
2026-09-23 02:27:11 -0700

    Remove Vite from admin production layout

--- resources/views/manager/layouts/admin.blade.php
+++ resources/views/manager/layouts/admin.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}?v={{ filemtime(public_path('assets/css/app.css')) }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
